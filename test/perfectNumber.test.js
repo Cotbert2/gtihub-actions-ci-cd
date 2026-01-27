d1fb42df4c10e7220e30151e95929efdfb272aef
Cotbert2 <mtaeojgdev@gmail.com>
2026-01-27 12:27:03 -0500

    perfect number

--- test/perfectNumber.test.js
+++ test/perfectNumber.test.js
@@ -1,22 +1,21 @@
-const { PerfectNumber } = require('../perfectNumber');
+const { isPerfect } = require('../perfectNumber');
 
 describe('PerfectNumber', () => {
-    const perfectNumber = new PerfectNumber();
     it('should return true for perfect number 6', () => {
-        expect(perfectNumber.isPerfect(6)).toBe(true);
+        expect(isPerfect(6)).toBe(true);
     });
 
     //
     it('should return true for perfect number 28', () => {
-        expect(perfectNumber.isPerfect(28)).toBe(true);
+        expect(isPerfect(28)).toBe(true);
     });
 
     it('should return false for non-perfect number 10', () => {
-        expect(perfectNumber.isPerfect(10)).toBe(false);
+        expect(isPerfect(10)).toBe(false);
     });
 
     it('should return false for non-perfect number 0', () => {
-        expect(perfectNumber.isPerfect(0)).toBe(false);
+        expect(isPerfect(0)).toBe(false);
     });
 
 });
