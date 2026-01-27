ac6aea3f08e1b5a0c785816dc0c5aee1d74eae41
Cotbert2 <mtaeojgdev@gmail.com>
2026-01-27 12:31:05 -0500

    fixed test

--- perfectNumber.js
+++ perfectNumber.js
@@ -1,4 +1,4 @@
-const isPerfect = (num ) => {
+const isPerfect = (num) => {
         if (num <= 1) return false;
         let sum = 1; // 1 is a proper divisor
         for (let i = 2; i <= Math.sqrt(num); i++) {
